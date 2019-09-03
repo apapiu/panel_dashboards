b56f6a14fb52a587a6a85e9cb0c22f1d429f7bf4
Alex Papiu <alexpapiu@Alexs-MacBook-Pro.local>
2019-09-03 00:53:15 -0400

    add snp 500 market app

--- panelserverextension.py
+++ panelserverextension.py
@@ -2,4 +2,4 @@ from subprocess import Popen
 
 def load_jupyter_server_extension(nbapp):
     """serve the bokeh-app directory with bokeh server"""
-    Popen(["panel", "serve", "apps/mean_filter.ipynb", "--allow-websocket-origin=*"])
+    Popen(["panel", "serve", "apps/s_and_p_500.ipynb", "--allow-websocket-origin=*"])
